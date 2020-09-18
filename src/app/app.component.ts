71f5938583f37da4ab653b7a9eaca219f71a617c
Sam <samtpomarico@gmail.com>
2020-09-18 16:06:15 -0500

    Completed part 6 - highlight space debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
- 
+          
           let fetchedSatellites = data.satellites;
           for (let i = 0; i < fetchedSatellites.length; i++) {
             let newSatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);  
