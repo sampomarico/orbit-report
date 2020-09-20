90bb5b6fd50536e99cd58f36cc338ff5bad9a9d2
Sam <samtpomarico@gmail.com>
2020-09-20 10:12:54 -0500

    Completed parts 7 and 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,15 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
+  displayList: Satellite[];
 
   sourceList: Satellite[];
 
   constructor() {
+
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    this.displayList=[];
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -22,8 +25,20 @@ export class AppComponent {
           for (let i = 0; i < fetchedSatellites.length; i++) {
             let newSatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);  
             this.sourceList.push(newSatellite);
-          }
+          } 
+          this.displayList = this.sourceList.slice(0);
           }.bind(this));
       }.bind(this));
+    }
+    search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+        let name = this.sourceList[i].name.toLocaleLowerCase();
+        if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+        }
+      }
+      this.displayList = matchingSatellites;
     }
   }
